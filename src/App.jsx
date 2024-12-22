2d01be23c11ac48e1f0e3cf4bbef1cf75f1e26d3
prathmesh ladkat <prathameshladkat336@gmail.com>
2024-12-22 13:24:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+//import "./App.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Body from "./components/Body";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
+
+function App() {
+  return (
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
+}
+
+export default App;
